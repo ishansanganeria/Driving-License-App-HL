f3b4e70325c2cc6af43d8ab2a4665fee4b3ba2ac
Ishan Sanganeria <ishansanganeria7@gmail.com>
2019-02-16 01:05:26 +0530

    Reverted to example.com

--- Mark 1/scripts/cli_script.sh
+++ Mark 1/scripts/cli_script.sh
@@ -2,4 +2,4 @@
 
 export CHANNEL_NAME=mychannel
 
-peer channel create -o orderer.seva.gov.in:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/seva.gov.in/orderers/orderer.seva.gov.in/msp/tlscacerts/tlsca.seva.gov.in-cert.pem
+peer channel create -o orderer.example.com:7050 -c $CHANNEL_NAME -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
