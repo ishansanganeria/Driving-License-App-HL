ea7523e788bfcac28c970005437133f68936e217
Ishan Sanganeria <ishansanganeria7@gmail.com>
2019-02-16 00:20:17 +0530

    .

--- Mark 1/script.sh
+++ Mark 1/script.sh
@@ -11,11 +11,32 @@ then
     service docker restart
 fi
 
+
+function clearContainers() {
+  CONTAINER_IDS=$(docker ps -a | awk '($2 ~ /dev-peer.*.mycc.*/) {print $1}')
+  if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" == " " ]; then
+    echo "---- No containers available for deletion ----"
+  else
+    docker rm -f $CONTAINER_IDS
+  fi
+}
+
+function removeUnwantedImages() {
+  DOCKER_IMAGE_IDS=$(docker images | awk '($1 ~ /dev-peer.*.mycc.*/) {print $3}')
+  if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" == " " ]; then
+    echo "---- No images available for deletion ----"
+  else
+    docker rmi -f $DOCKER_IMAGE_IDS
+  fi
+}
+
 # REMOVE AND PREVIOUSLY EXISITING CRYPTOGRAPHIC MATERIAL
 rm -rf crypto-config
 rm -rf channel-artifacts
 
-docker-compose -f docker-compose-cli.yaml -f docker-compose-couch.yaml down --volumes
+docker-compose -f docker-compose-cli.yaml -f docker-compose-couch.yaml down --volumes --remove-orphans
+clearContainers
+removeUnwantedImages
 
 docker rm -f $(docker ps -aq) > /dev/null 2>&1
 docker network rm $(docker network ls -q)
@@ -52,7 +73,8 @@ echo "#################     Running docker containers     ##################"
 echo "######################################################################"
 echo
 
-docker-compose -f docker-compose-cli.yaml -f docker-compose-couch.yaml up -d
+docker-compose -f docker-compose-cli.yaml up -d
+# docker-compose -f docker-compose-cli.yaml -f docker-compose-couch.yaml up -d
 
 echo 
 
