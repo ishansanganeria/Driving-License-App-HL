fe5d5d2948a9b7c26c62f6c60303928ccb738caf
ISHANSANGANERIA\ishan <ishansanganeria7@gmail.com>
2019-02-25 16:35:03 +0530

    Mark2

---
+++ Mark2/basic-network/start.sh
@@ -0,0 +1,13 @@
+set -ev
+
+bash generate.sh
+
+docker-compose -f docker-compose.yml down
+
+docker-compose -f docker-compose.yml up -d ca.example.com orderer.example.com peer0.orgdl.example.com couchdb
+
+export FABRIC_START_TIMEOUT=1
+sleep ${FABRIC_START_TIMEOUT}
+
+docker exec -e "CORE_PEER_LOCALMSPID=OrgdlMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@orgdl.example.com/msp" peer0.orgdl.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
+docker exec -e "CORE_PEER_LOCALMSPID=OrgdlMSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@orgdl.example.com/msp" peer0.orgdl.example.com peer channel join -b mychannel.block
