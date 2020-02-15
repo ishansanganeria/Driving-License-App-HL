db6b0cb382c8114474429c0c9ad316866e03a0ca
Ishan <isanganeria@mast.com>
2020-02-15 16:26:54 +0530

    Mark 2 to Root Folder

--- Mark 1/scripts/cli_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-export CHANNEL_BOTH=mychannel
-
-peer channel create -o orderer.example.com:7050 -c $CHANNEL_BOTH -f ./channel-artifacts/channel.tx --tls --cafile /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
